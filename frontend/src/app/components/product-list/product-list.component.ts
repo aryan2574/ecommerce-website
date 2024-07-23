3156f5fb126932c5408116ac44debc3caf778742
aryan2574 <ayush.aryan94@yahoo.com>
2024-07-23 20:12:13 +0530

    Implemented routing for product books and coffee mugs

--- frontend/src/app/components/product-list/product-list.component.ts
+++ frontend/src/app/components/product-list/product-list.component.ts
@@ -2,36 +2,52 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductService } from '../../services/product.service';
 import { Product } from '../../common/product';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './product-list-grid.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
   products: Product[] = [];
   errorMessage: string = '';
-  
-  constructor(private productService: ProductService) {}
+
+  currentCategoryId: number = 1;
+
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    this.listProducts();
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    });
   }
 
   listProducts(): void {
-    this.productService.getProductList().subscribe({
-      next: data => {
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if (hasCategoryId) {
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+    } else {
+      this.currentCategoryId = 1;
+    }
+
+    this.productService.getProductList(this.currentCategoryId).subscribe({
+      next: (data) => {
         this.products = data;
         console.log('Data array:', this.products); // Log the data array
       },
-      error: err => {
+      error: (err) => {
         console.error('Error occurred: ', err); // Log any error
       },
       complete: () => {
         console.log('Request completed'); // Log completion of the request
-      }
+      },
     });
   }
 }
