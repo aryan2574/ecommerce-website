68c8ac2776f4b06fe6fe4c1e1499b1d8b7f3d2e2
aryan2574 <ayush.aryan94@yahoo.com>
2024-07-27 21:00:04 +0530

    Implemented routing to different products

--- frontend/src/app/components/product-list/product-list.component.ts
+++ frontend/src/app/components/product-list/product-list.component.ts
@@ -3,19 +3,19 @@ import { CommonModule } from '@angular/common';
 import { ProductService } from '../../services/product.service';
 import { Product } from '../../common/product';
 import { ActivatedRoute } from '@angular/router';
+import { ProductCategory } from '../../common/product-category';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './product-list-grid.component.html',
+  templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
   products: Product[] = [];
-  errorMessage: string = '';
-
   currentCategoryId: number = 1;
+  currentCategoryName: string = 'Books';
 
   constructor(
     private productService: ProductService,
@@ -32,22 +32,26 @@ export class ProductListComponent implements OnInit {
     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
 
     if (hasCategoryId) {
+      // get the "id" param string. convert string to a number using the "+" symbol
       this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
     } else {
+      // not category id available ... default to category id 1
       this.currentCategoryId = 1;
+      this.currentCategoryName = 'Books';
     }
 
-    this.productService.getProductList(this.currentCategoryId).subscribe({
-      next: (data) => {
-        this.products = data;
-        console.log('Data array:', this.products); // Log the data array
-      },
-      error: (err) => {
-        console.error('Error occurred: ', err); // Log any error
-      },
-      complete: () => {
-        console.log('Request completed'); // Log completion of the request
-      },
-    });
+    this.productService
+      .getProductList(this.currentCategoryId)
+      .subscribe((data: Product[]) => (this.products = data));
+
+    this.productService
+      .getProductCategories()
+      .subscribe((data: ProductCategory[]) => {
+        for (let category of data) {
+          if (category.id === this.currentCategoryId) {
+            this.currentCategoryName = category.categoryName;
+          }
+        }
+      });
   }
 }
--- frontend/src/app/services/product.service.ts
+++ frontend/src/app/services/product.service.ts
@@ -3,10 +3,17 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators'; // Import map operator
 import { Product } from '../common/product'; // Adjust the import path if needed
+import { ProductCategory } from '../common/product-category';
 
-interface GetResponse {
+interface GetResponseProducts {
   _embedded: {
     product: Product[];
+  };
+}
+
+interface GetResponseProductCategory {
+  _embedded: {
+    productCategory: ProductCategory[];
   };
 }
 
@@ -14,15 +21,22 @@ interface GetResponse {
   providedIn: 'root',
 })
 export class ProductService {
-  private baseUrl = 'http://localhost:8080/api/product';
+  private productURL = 'http://localhost:8080/api/product';
+  private productCategoryURL = 'http://localhost:8080/api/product-category';
 
   constructor(private httpClient: HttpClient) {}
 
   getProductList(theCategoryId: number): Observable<Product[]> {
-    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
+    const searchUrl = `${this.productURL}/search/findByCategoryId?id=${theCategoryId}`;
 
     return this.httpClient
-      .get<GetResponse>(searchUrl)
+      .get<GetResponseProducts>(searchUrl)
       .pipe(map((response) => response._embedded.product));
+  }
+
+  getProductCategories(): Observable<ProductCategory[]> {
+    return this.httpClient
+      .get<GetResponseProductCategory>(this.productCategoryURL)
+      .pipe(map((response) => response._embedded.productCategory));
   }
 }
