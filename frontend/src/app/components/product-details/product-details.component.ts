34adf666311f7fe030437f3d0ae75615bf7f7368
aryan2574 <ayush.aryan94@yahoo.com>
2024-08-26 13:26:04 +0530

    Implemented product adding to cart feature to product details component

--- frontend/src/app/components/product-details/product-details.component.ts
+++ frontend/src/app/components/product-details/product-details.component.ts
@@ -3,6 +3,8 @@ import { Product } from '../../common/product';
 import { ProductService } from '../../services/product.service';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { CartService } from '../../services/cart.service';
+import { CartItem } from '../../common/cart-item';
 
 @Component({
   selector: 'app-product-details',
@@ -16,6 +18,7 @@ export class ProductDetailsComponent implements OnInit {
 
   constructor(
     private productService: ProductService,
+    private cartService: CartService,
     private route: ActivatedRoute
   ) {}
 
@@ -32,5 +35,20 @@ export class ProductDetailsComponent implements OnInit {
     this.productService.getProduct(productId).subscribe((data: Product) => {
       this.product = data;
     });
+  }
+
+  addToCart() {
+    console.log(
+      `Adding to cart: ${this.product.name}, ${this.product.unitPrice}`
+    );
+
+    const cartItem = new CartItem(
+      this.product.id,
+      this.product.name,
+      this.product.imageUrl,
+      this.product.unitPrice
+    );
+
+    this.cartService.addToCart(cartItem);
   }
 }
