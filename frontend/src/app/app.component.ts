f344895313d19ab5715864023b7a7419efe5e71d
aryan2574 <ayush.aryan94@yahoo.com>
2024-08-27 18:10:27 +0530

    Added feature to handle when cart is empty

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
 import { SearchComponent } from './components/search/search.component';
@@ -10,6 +10,7 @@ import { CartStatusComponent } from './components/cart-status/cart-status.compon
   standalone: true,
   imports: [
     RouterOutlet,
+    RouterLink,
     ProductListComponent,
     ProductCategoryMenuComponent,
     SearchComponent,
