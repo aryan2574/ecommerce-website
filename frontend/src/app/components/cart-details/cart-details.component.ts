0a719e28a10447cb7a11e890b613fa965270181c
aryan2574 <ayush.aryan94@yahoo.com>
2024-08-27 18:02:37 +0530

    Added checkout cart total price feature

--- frontend/src/app/components/cart-details/cart-details.component.ts
+++ frontend/src/app/components/cart-details/cart-details.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartItem } from '../../common/cart-item';
+import { CartService } from '../../services/cart.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cart-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cart-details.component.html',
-  styleUrl: './cart-details.component.css'
+  styleUrl: './cart-details.component.css',
 })
-export class CartDetailsComponent {
+export class CartDetailsComponent implements OnInit {
+  cartItems: CartItem[] = [];
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
 
+  constructor(private cartService: CartService) {}
+
+  ngOnInit(): void {
+    this.listCartDetails();
+  }
+
+  listCartDetails() {
+    // Get a handle to the cart items
+    this.cartItems = this.cartService.cartItems;
+
+    // subscribe to the cart totalPrice
+    this.cartService.totalPrice.subscribe(
+      (data: any) => (this.totalPrice = data)
+    );
+
+    // subscribe to the cart totalQuantity
+    this.cartService.totalQuantity.subscribe(
+      (data: any) => (this.totalQuantity = data)
+    );
+
+    this.cartService.computeCartTotals();
+  }
 }
