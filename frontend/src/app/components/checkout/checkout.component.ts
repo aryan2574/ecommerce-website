6787451640874fb61dd18b4e92bff22b4550628f
aryan2574 <ayush.aryan94@yahoo.com>
2024-08-28 18:32:49 +0530

    Added data forms

--- frontend/src/app/components/checkout/checkout.component.ts
+++ frontend/src/app/components/checkout/checkout.component.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './checkout.component.html',
-  styleUrl: './checkout.component.css'
+  styleUrls: ['./checkout.component.css'],
 })
-export class CheckoutComponent {
+export class CheckoutComponent implements OnInit {
+  checkoutFormGroup: FormGroup = this.formBuilder.group({});
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.checkoutFormGroup = this.formBuilder.group({
+      customer: this.formBuilder.group({
+        firstName: ['', Validators.required],
+        lastName: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+      }),
+      shippingAddress: this.formBuilder.group({
+        street: ['', Validators.required],
+        city: ['', Validators.required],
+        state: ['', Validators.required],
+        country: ['', Validators.required],
+        zipCode: ['', Validators.required],
+      }),
+      billingAddress: this.formBuilder.group({
+        street: ['', Validators.required],
+        city: ['', Validators.required],
+        state: ['', Validators.required],
+        country: ['', Validators.required],
+        zipCode: ['', Validators.required],
+      }),
+      creditCard: this.formBuilder.group({
+        cardType: ['', Validators.required],
+        nameOnCard: ['', Validators.required],
+        cardNumber: ['', Validators.required],
+        securityCode: ['', Validators.required],
+        expirationMonth: ['', Validators.required],
+        expirationYear: ['', Validators.required],
+      }),
+    });
+  }
+
+  onSubmit() {
+    console.log('Submitted');
+    console.log(this.checkoutFormGroup.get('customer')?.value);
+  }
 }
