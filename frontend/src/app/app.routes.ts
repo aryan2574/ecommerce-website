76ac67bce585b86000b2d9f1aef24ce90c85f0de
aryan2574 <ayush.aryan94@yahoo.com>
2024-09-04 14:37:20 +0530

    Implemented routes for Member component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,12 +1,27 @@
-import { Routes } from '@angular/router';
+import { Router, Routes } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
-import { OktaCallbackComponent } from '@okta/okta-angular';
+import { OktaAuthGuard, OktaCallbackComponent } from '@okta/okta-angular';
 import { LoginComponent } from './components/login/login.component';
+import { MembersPageComponent } from './components/members-page/members-page.component';
+import { Injector } from '@angular/core';
+
+function sendToLoginPage(injector: Injector) {
+  const router = injector.get(Router);
+  router.navigate(['/login']);
+}
 
 export const routes: Routes = [
+  {
+    path: 'members',
+    component: MembersPageComponent,
+    canActivate: [OktaAuthGuard],
+    data: {
+      onAuthRequired: sendToLoginPage,
+    },
+  },
   { path: 'login/callback', component: OktaCallbackComponent },
   { path: 'login', component: LoginComponent },
   { path: 'checkout', component: CheckoutComponent },
